456a2a5bc85eff9afa1d31ec5027f18a038e878e
Burning woods <tabc20030607@outlook.com>
2026-04-24 13:39:01 +0800

    部署路径更新

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: process.env['BASE_URL'] || '/',
   plugins: [react(), tailwindcss()],
-
+  preview: {
+    allowedHosts: ['lotusai.extrotec.com', 'localhost'],
+  },
 })
